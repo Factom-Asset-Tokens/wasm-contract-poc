b9fd31ab35363eaeaf3734cb13f9c4685c61f481
Devon Katz <devonk@dbgrow.com>
2019-06-03 19:50:51 -0700

    Add example add.c function, compilation to WASM, tests

---
+++ examples/add/add.c
@@ -0,0 +1,9 @@
+int add(int x, int y) {
+  return x + y;
+}
+
+int running = 0;
+int addRunning(int x){
+  running +=x;
+  return running;
+}
---
+++ examples/add/test/add.unit.spec.js
@@ -0,0 +1,50 @@
+const assert = require('chai').assert;
+const fs = require('fs');
+const path = require("path");
+
+describe('C: Add Two Numbers', function () {
+
+    let instance;
+    it('Instantiate WASM File', async function () {
+        const memory = new WebAssembly.Memory({initial: 256, maximum: 256});
+        const imports = {
+            env: {
+                abortStackOverflow: () => {
+                    throw new Error('overflow');
+                },
+                table: new WebAssembly.Table({initial: 0, maximum: 0, element: 'anyfunc'}),
+                __table_base: 0,
+                memory: memory,
+                __memory_base: 1024,
+                STACKTOP: 0,
+                STACK_MAX: memory.buffer.byteLength,
+            }
+        };
+
+        const buffer = fs.readFileSync(path.resolve(__dirname, '../build/add.wasm'));
+        const result = await WebAssembly.instantiate(buffer, imports);
+        instance = result.instance;
+
+        assert.isFunction(instance.exports._add);
+        assert.isFunction(instance.exports._addRunning);
+    });
+
+    it('Add Two Numbers', async function () {
+        //test addition
+        assert.strictEqual(instance.exports._add(5, 31), 36); //add two numbers
+        assert.strictEqual(instance.exports._add(-5, 5), 0); //add signed numbers
+
+        //test running addition
+        assert.strictEqual(instance.exports._addRunning(99), 99); //add 99 to the running balance, result should be 99
+        assert.strictEqual(instance.exports._addRunning(1), 100); //add 1 to the running balance, result should be 100
+
+        //check for addition mistakes
+        assert.notStrictEqual(instance.exports._add(5, 31), 37);
+
+        //Demonstrate how passing the wrong type or no type is ignored
+        //This will not throw an error, instead the default C int value is used (0)
+        assert.doesNotThrow(() => instance.exports._add(5, "A"));
+        assert.doesNotThrow(() => instance.exports._add(5, 3.501));
+        assert.doesNotThrow(() => instance.exports._add(5));
+    });
+});
