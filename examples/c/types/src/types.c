a7ce1738f4e1821aac957f4a9ad9fd6af1a3d35c
Devon Katz <devonk@dbgrow.com>
2019-06-07 20:11:26 -0700

    Add examples for working with ints, booleans, reading strings

--- examples/c/types/src/types.c
+++ examples/c/types/src/types.c
@@ -1,3 +1,75 @@
+/*
+
+Integers
+
+*/
+int getTen(){
+  return 10;
+}
+
+int add(int x, int y){
+  return x + y;
+}
+
+int mult(int x, int y){
+  return x * y;
+}
+
+/*
+
+Floating Points
+
+NOTE: You must use the double return type to get expected return precision calling from JS
+Otherwise you will get something like  10.010000228881836.... which is no good
+
+*/
+
+double getTenPointZeroOne(){
+  return 10.01;
+}
+
+double addFloats(double *x){
+//return x;
+return 0.01;
+}
+
+/*
+
+Booleans
+
+*/
+
+int getTrue(){
+  return 1;
+}
+
+int getFalse(){
+  return 0;
+}
+
+int echoBoolean(int x){
+  return x;
+}
+
+int negate(int x){
+  return !x;
+}
+
+int or(int x,int y){
+  return x | y;
+}
+
+/*
+
+Strings
+
+*/
+
 char * getString() {
   return "It's Alive!!!";
+}
+
+char * echoString(char *x) {
+  x[0]='a';
+  return x;
 }
--- examples/c/types/test/types.unit.spec.js
+++ examples/c/types/test/types.unit.spec.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const util = require('../../../../src/util');
 
-describe('C WASM: Add Two Numbers', function () {
+describe('C WASM: Use Types from JS', function () {
 
     let wasm;
     let memory;
@@ -16,21 +16,103 @@ describe('C WASM: Add Two Numbers', function () {
         wasm.imports = imports;
 
         assert.isFunction(wasm.instance.exports._getString);
+        assert.isFunction(wasm.instance.exports._echoString);
     });
 
-    it('Get A String - No Params', async function () {
+    describe('Integers', function () {
+        it('Get Ten', async function () {
+            assert.strictEqual(wasm.instance.exports._getTen(), 10);
+        });
 
-        //get a handle on the WASM module's memory
-        memory = new Uint8Array(wasm.imports.env.memory.buffer);
+        it('Add Ints', async function () {
+            assert.strictEqual(wasm.instance.exports._add(3, 99), 102);
+        });
 
-        //get the pointer in memory resulting from the call (int)
-        const pointer = wasm.instance.exports._getString();
+        it('Mult Ints', async function () {
+            assert.strictEqual(wasm.instance.exports._mult(3, 99), 297);
+        });
+    });
 
-        //read off the memory belonging to that pointer encoded to UTF-8
-        const string = getUTF8String(memory, pointer);
+    describe('Floating Point Numbers', function () {
+        it('Get Float', async function () {
+            assert.strictEqual(wasm.instance.exports._getTenPointZeroOne(), 10.01);
+        });
 
-        //verify our message is expected
-        assert.strictEqual(string, "It's Alive!!!");
+        it('Add Floats', async function () {
+            // assert.strictEqual(wasm.instance.exports._addFloats(0.1,101), 101.1);
+        });
+    });
+
+    describe('Booleans', function () {
+        it('Get True', async function () {
+            const truthy = wasm.instance.exports._getTrue();
+            assert.strictEqual(Boolean(truthy).valueOf(), true);
+        });
+
+        it('Get False', async function () {
+            const falsy = wasm.instance.exports._getFalse();
+            assert.strictEqual(Boolean(falsy).valueOf(), false);
+        });
+
+        it('Echo', async function () {
+            const truthy = wasm.instance.exports._echoBoolean(true);
+            assert.strictEqual(Boolean(truthy).valueOf(), true);
+
+            const falsy = wasm.instance.exports._echoBoolean(false);
+            assert.strictEqual(Boolean(falsy).valueOf(), false);
+        });
+
+        it('Negate', async function () {
+            const negate = wasm.instance.exports._negate(true);
+            assert.strictEqual(Boolean(negate).valueOf(), false);
+        });
+
+        it('Or', async function () {
+            let truthy = wasm.instance.exports._or(true, true);
+            assert.strictEqual(Boolean(truthy).valueOf(), true);
+
+            truthy = wasm.instance.exports._or(true, false);
+            assert.strictEqual(Boolean(truthy).valueOf(), true);
+
+            truthy = wasm.instance.exports._or(false, true);
+            assert.strictEqual(Boolean(truthy).valueOf(), true);
+
+            const falsy = wasm.instance.exports._or(false, false);
+            assert.strictEqual(Boolean(falsy).valueOf(), false);
+        });
+    });
+
+    describe('Strings', function () {
+        it('Get A String - No Params', async function () {
+
+            //get a handle on the WASM module's memory
+            memory = new Uint8Array(wasm.imports.env.memory.buffer);
+
+            //get the pointer in memory resulting from the call (int)
+            const pointer = wasm.instance.exports._getString();
+
+            //read off the memory belonging to that pointer encoded to UTF-8
+            const string = getUTF8String(memory, pointer);
+
+            //verify our message is expected
+            assert.strictEqual(string, "It's Alive!!!");
+        });
+
+        it('Echo A String', async function () {
+
+            //get a handle on the WASM module's memory
+            memory = new Uint8Array(wasm.imports.env.memory.buffer);
+
+            //get the pointer in memory resulting from the call (int)
+            const pointer = wasm.instance.exports._echoString("ppp");
+
+            //read off the memory belonging to that pointer encoded to UTF-8
+            const string = getUTF8String(memory, pointer);
+
+            console.log('PTR:', pointer, 'Got', string);
+
+            assert.isString(string);
+        });
     });
 });
 
