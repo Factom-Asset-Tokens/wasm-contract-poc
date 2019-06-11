3c2a9ad06d1d761d51034c89f4c58ca7a6b9fd4d
Devon Katz <devonk@dbgrow.com>
2019-06-11 13:26:48 -0700

    Add example of using AS WASM from native WASM loader instead of AS loader

--- examples/assemblyscript/add/test/add.unit.spec.js
+++ examples/assemblyscript/add/test/add.unit.spec.js
@@ -7,8 +7,58 @@ const loader = require("assemblyscript/lib/loader");
 
 describe('AssemblyScript WASM: Add Two Numbers', function () {
 
+    it('Instantiate WASM File - Native WebAssembly', async function () {
+        const memory = new WebAssembly.Memory({initial: 256, maximum: 256});
+        const imports = {
+            env: {
+                abortStackOverflow: () => {
+                    throw new Error('overflow');
+                },
+                table: new WebAssembly.Table({initial: 0, maximum: 0, element: 'anyfunc'}),
+                __table_base: 0,
+                memory: memory,
+                __memory_base: 1024,
+                STACKTOP: 0,
+                STACK_MAX: memory.buffer.byteLength,
+                abort: function (a) {
+                    if (a) console.error('ABORT:', a);
+                    else console.log('ABORT');
+                }
+            }
+        };
+
+        const buffer = fs.readFileSync(path.resolve(__dirname, '../../../assemblyscript/add/build/add.wasm'));
+        const wasmModule = new WebAssembly.Module(buffer);
+        const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
+
+        assert.strictEqual(wasmInstance.exports.add(1, 20), 21);
+
+        //You can also use the built in memory allocation & deallocation functions
+        //that are injected into AssemblyScript binaries if using a module built by AS
+
+        //allocate a 32 Byte space in the module's memory and get the pointer
+        const pointer0 = wasmInstance.exports.__retain(wasmInstance.exports.__alloc(32));
+
+        //and again
+        const pointer1 = wasmInstance.exports.__retain(wasmInstance.exports.__alloc(32));
+
+        //check that our memory was allocated in different segments
+        assert.isAbove(pointer1, pointer0);
+
+        //deallocate our memory
+        wasmInstance.exports.__release(pointer0);
+        wasmInstance.exports.__release(pointer1);
+
+        //allocate another space
+        const pointer2 = wasmInstance.exports.__retain(wasmInstance.exports.__alloc(32));
+
+        //check that our old memory from pointer 0 and 1 were released
+        //if the new pointer is equal to the very virst the we're good!
+        assert.strictEqual(pointer2, pointer0);
+    });
+
     let module;
-    it('Instantiate WASM File', async function () {
+    it('Instantiate WASM File - AssemblyScript Loader', async function () {
         const imports = util.getDefaultImports();
 
         const buffer = fs.readFileSync(path.resolve(__dirname, '../build/add.wasm'));
